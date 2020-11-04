0234b2eb96b3bbbe5511a4d5377b236f793fefb2
Leonid <leo1133407@list.ru>
2020-11-04 18:06:04 +0300

    last version, API_KEY dont work((

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
+    <the-loader v-if="isPreloaderVisible"/>
+
     <div class="container">
       <div class="header d-flex justify-content-between">
 
         <div>
-          <h2>Омск</h2>
+          <h2>{{weatherData.name}}</h2>
 
           <div class="d-flex align-items-center">
             <p class="opacity">Сменить город</p>
 
-            <div class="header-location d-flex align-items-center">
+            <div class="header-location d-flex align-items-center" @click="getPosition">
               <img src="@/assets/img/location.svg" alt="">
               <p class="opacity">Мое местоположение</p>
             </div>
@@ -26,7 +28,9 @@
                     type="radio"
                     name="radio"
                     id="radio1"
+                    value="metric"
                     checked
+                    @change="radioChange($event.target.value)"
                 />
                 <label for="radio1">C</label>
               </div>
@@ -36,6 +40,8 @@
                     type="radio"
                     name="radio"
                     id="radio2"
+                    value="imperial"
+                    @change="radioChange($event.target.value)"
                 />
                 <label for="radio2">F</label>
               </div>
@@ -49,41 +55,98 @@
         <div>
           <div class="d-flex align-items-center">
             <img src="@/assets/img/cloudy.svg" alt="">
-            <h1>19º</h1>
+            <h1>{{weatherData.main.temp}}º</h1>
           </div>
 
-          <h4 class="middle-description">Преимущественно солнечно</h4>
+          <h4 class="middle-description">{{weatherData.weather[0].description}}</h4>
         </div>
       </div>
 
       <div class="footer">
         <div>
           <p class="opacity">Ветер</p>
-          <h4 class="footer-value">5 м/c, западный</h4>
+          <h4 class="footer-value">{{weatherData.wind.speed}} м/c, {{countSide}}</h4>
         </div>
 
         <div>
           <p class="opacity">Давление</p>
-          <h4 class="footer-value">752 мм рт. ст.</h4>
+          <h4 class="footer-value">{{weatherData.main.pressure}} мм рт. ст.</h4>
         </div>
 
         <div>
           <p class="opacity">Влажность</p>
-          <h4 class="footer-value">60%</h4>
+          <h4 class="footer-value">{{weatherData.main.humidity}}%</h4>
         </div>
 
-        <div>
-          <p class="opacity">Вероятность дождя</p>
-          <h4 class="footer-value">10%</h4>
-        </div>
+        <!--        <div>-->
+        <!--          <p class="opacity">Вероятность дождя</p>-->
+        <!--          <h4 class="footer-value">10%</h4>-->
+        <!--        </div>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  import TheLoader from './components/TheLoader'
+
   export default {
     name: 'App',
+    components: { TheLoader },
+    computed: {
+      ...mapState(['weatherData', 'isPreloaderVisible']),
+      countSide() {
+        const deg = this.weatherData.wind.deg
+        let degDescription = ''
+
+        if (deg >= 340 && deg <= 20) {
+          degDescription = 'северный'
+        } else if (deg > 20 && deg < 70) {
+          degDescription = 'северо-восточный'
+        } else if (deg >= 70 && deg <= 110) {
+          degDescription = 'восточный'
+        } else if (deg > 110 && deg < 160) {
+          degDescription = 'южно-восточный'
+        } else if (deg >= 160 && deg <= 200) {
+          degDescription = 'южный'
+        } else if (deg > 200 && deg < 250) {
+          degDescription = 'южно-западный'
+        } else if (deg >= 250 && deg <= 290) {
+          degDescription = 'западный'
+        } else if (deg > 290 && deg < 340) {
+          degDescription = 'северо-западный'
+        }
+
+        return degDescription
+      },
+    },
+    methods: {
+      radioChange(value) {
+        this.$store.commit('CHANGE_UNITS_DATA', value)
+        this.fetchWeatherData()
+      },
+      fetchWeatherData() {
+        this.$store.dispatch('fetchWeather')
+      },
+      getPosition() {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const coords = position.coords
+
+            this.$store.commit('CHANGE_CITY_COORDS', {
+              lat: coords.latitude,
+              lon: coords.longitude,
+            })
+
+            this.fetchWeatherData()
+          },
+        )
+      },
+    },
+    mounted() {
+      this.fetchWeatherData()
+    },
   }
 </script>
 
@@ -102,6 +165,7 @@
     margin-top: 75px;
 
     &-location {
+      cursor: pointer;
       margin-left: 29px;
     }
 
@@ -148,7 +212,7 @@
     margin-top: 190px;
 
     &-description {
-      margin-left: 40px;
+      text-align: center;
     }
   }
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,48 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    city: 'Krasnodar',
+    API_KEY: '799b31d237458c762b4ea6d23f2e5d1c',
+    units: 'metric',
+    weatherData: {},
+    isPreloaderVisible: false,
+  },
+  mutations: {
+    CHANGE_WEATHER_DATA(state, data) {
+      state.weatherData = data
+    },
+    CHANGE_UNITS_DATA(state, data) {
+      state.units = data
+    },
+    CHANGE_CITY_COORDS(state, data) {
+      state.cityCoords = data
+    },
+    CHANGE_PRELOADER_VISIBLE(state, data) {
+      state.isPreloaderVisible = data
+    },
+  },
+  actions: {
+    async fetchWeather({ state, commit }) {
+      commit('CHANGE_PRELOADER_VISIBLE', true)
+
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast/daily?
+          q=${state.city}
+          &lang=ru
+          &appid=${state.API_KEY}
+          &units=${state.units}`,
+        )
+        const data = await res.json()
+
+        commit('CHANGE_WEATHER_DATA', data)
+        commit('CHANGE_PRELOADER_VISIBLE', false)
+      } catch (e) {
+        console.log(e)
+        commit('CHANGE_PRELOADER_VISIBLE', false)
+        throw e
+      }
+    },
+  },
 })
